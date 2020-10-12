332b3d1baae3207854430667df2604560caee784
Vivica Cong Ma <vivicacongma@gmail.com>
2020-10-12 21:06:44 +0000

    Automatically backed up by Learn

--- lib/10001st_prime.rb
+++ lib/10001st_prime.rb
@@ -6,27 +6,13 @@ def is_prime?(n)
   true
 end 
 
-# def prime_number_for(nth_element)
-#   count = 0 
-#   number = 0 
-#   while count != nth_element
-#     count += 1 if is_prime?(number)
-#   end 
-#   number
-# end 
-# def is_prime?(number)
-#   return false if number.zero? || number == 1
-#   integers =* (2..Math.sqrt(number).round)
-#   integers.each {|i| return false if (number % i).zero?}
-#   true
-# end
-
 def prime_number_for(nth_element)
-  count = 0
-  number = 0
+  count = 0 
+  number = 0 
   while count != nth_element
-    number += 1
+    number +=1
     count += 1 if is_prime?(number)
-  end
+  end 
   number
-end
+end 
+
