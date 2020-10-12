5a39f972293016a9bdb35113eb28e5c4eae551f9
Vivica Cong Ma <vivicacongma@gmail.com>
2020-10-12 21:17:47 +0000

    Automatically backed up by Learn

--- lib/oo_10001st_prime.rb
+++ lib/oo_10001st_prime.rb
@@ -1,14 +1,26 @@
 # Implement your object-oriented solution here!
 class Prime 
-  attr_accessor :integer
+  attr_accessor :number, nth_element
   
-  def initialize
-    @integer = integer
+  def initialize(nth_element)
+    @nth_element = nth_element
+    @number = prime_counter(nth_element)
   end 
   
-  def is_prime?(integer)
+  def is_prime?(number)
     return flase if integer.zero? || integer == 1
-    integer
+    integers *= (2..Math.sqrt(number).round)
+    integers.each{|i| return false if number % i == 0}
     true
+  end 
+  
+  def prime_counter(nth_element)
+    count = 0 
+    number = 0 
+    while count != nth_element
+      number +=1 
+      count += 1 if is_prime?(number)
+    end 
+    number
   end 
 end 
