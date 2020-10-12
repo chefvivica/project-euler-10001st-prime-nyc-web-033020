eef86372f2a9b88bab97f3d905315d499d587c15
Vivica Cong Ma <vivicacongma@gmail.com>
2020-10-12 21:12:02 +0000

    Automatically backed up by Learn

--- lib/oo_10001st_prime.rb
+++ lib/oo_10001st_prime.rb
@@ -1,1 +1,14 @@
 # Implement your object-oriented solution here!
+class Prime 
+  attr_accessor :integer
+  
+  def initialize
+    @integer = integer
+  end 
+  
+  def is_prime?(integer)
+    return flase if integer.zero? || integer == 1
+    integer
+    true
+  end 
+end 
