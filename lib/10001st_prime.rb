12a1f42db18dea87ecfab1ff330a99ae311645f3
Vivica Cong Ma <vivicacongma@gmail.com>
2020-10-12 21:05:15 +0000

    Automatically backed up by Learn

--- lib/10001st_prime.rb
+++ lib/10001st_prime.rb
@@ -1,16 +1,32 @@
 # Implement your procedural solution here!
-def is_prime?(n)
-  return false if n.zero? || n == 1 
-  integers =* (2..Math.sqrt(n).round)
-  integers.each{|i| return false  if (n % i).zero?}
+# def is_prime?(n)
+#   return false if n.zero? || n == 1 
+#   integers =* (2..Math.sqrt(n).round)
+#   integers.each{|i| return false  if (n % i).zero?}
+#   true
+# end 
+
+# def prime_number_for(nth_element)
+#   count = 0 
+#   number = 0 
+#   while count != nth_element
+#     count += 1 if is_prime?(number)
+#   end 
+#   number
+# end 
+def is_prime?(number)
+  return false if number.zero? || number == 1
+  integers =* (2..Math.sqrt(number).round)
+  integers.each {|i| return false if (number % i).zero?}
   true
-end 
+end
 
 def prime_number_for(nth_element)
-  count = 0 
-  number = 0 
+  count = 0
+  number = 0
   while count != nth_element
+    number += 1
     count += 1 if is_prime?(number)
-  end 
+  end
   number
-end 
+end
